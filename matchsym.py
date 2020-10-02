0604d5a38abd80671c3102280900089ca507c02f
ahmedrashad1639 <72245342+ahmedrashad1639@users.noreply.github.com>
2020-10-02 11:47:33 +0530

    Add files via upload

---
+++ matchsym.py
@@ -0,0 +1,52 @@
+from Stack import Stack
+def match(x,y):
+ if y==')' and x=='(':
+         return True
+ elif y=='}' and x=='{':
+    	return True
+ elif y==']'and x=='[':
+    	return True
+ return False
+
+def isMatched(expr):
+    S=Stack()
+    l=len(expr)
+    i=0
+    while i!=l:
+      symb=expr[i]
+      i=i+1
+      
+      if symb=='{' or symb=='[' or symb=='(':
+      	t=symb
+      	S.push(symb)
+      
+      elif symb=='}' or symb==']' or symb==')':
+       if S.isEmpty():
+      	    return False
+       else:
+      	   left=S.pop()
+       if not match(left,symb):
+         return False
+                 
+      
+
+    if S.isEmpty():
+    	return True
+    else:
+    	return False
+        
+
+    	
+    
+    
+def main():
+	expr = input('Enter the expression: ')
+	
+	if isMatched(expr):
+		print('Matched symbols')
+	else:
+		print('Unmatched symbols')
+
+if __name__ == '__main__':
+    main()
+
